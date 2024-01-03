46af46b5273eb86ecceae5dfb9ac68f901a9f7de
dev1 <dev1@email.com>
2024-01-03 13:27:58 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,21 @@
 resource "aws_instance" "web" {
+  #count = 11
+  count = length(var.instance_name)
   ami           = var.ami-id
-  instance_type = var.instance_type
-  #vpc_security_group_ids= [aws_security_group.roboshop.id]
-}
-
-
+  instance_type = var.instance_name[count.index] == "mongodb" || var.instance_name[count.index] == "mysql" || var.instance_name[count.index] == "shipping" ? "t3.small" : "t2.micro"
+  tags = {
+    Name = var.instance_name[count.index]
+  }
+  }
 
 #route 53 records
-# resource "aws_route53_record" "www" {
-#   zone_id = var.zone_id
-#   name    = var.ip-name
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_eip.lb.public_ip]
-# }
+  resource "aws_route53_record" "www" {
+  #count = 11
+  count = length(var.instance_name)
+  zone_id = var.zone_id
+  name    = "${var.instance_name[count.index]}.${var.ip-name}"
+  type    = "A"
+  ttl     = 1
+  records = [var.instance_name[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
+}
+
--- variables.tf
+++ variables.tf
@@ -1,14 +1,23 @@
 variable "ami-id" {
-  type    =  string
+  #type    =  string
   default =  "ami-03265a0778a880afb"
 }
 
-variable "instance_type" {
-  type    =  string
-  default =  "t2.micro"
+variable "instance_name" {
+  type    =  list
+  default =  ["mongodb","mysql","shipping","user","cart","reddis","rabbitmq","catalogue","payment","web","dispatch"]
+
 }
 
+variable "zone_id" {
+  #type    =  string
+  default =  "Z02572582AYLBU30KSIJ8"
+}
 
+variable "ip-name" {
+  #type    =  string
+  default =  "daws76.online"
+}
 
 
 
