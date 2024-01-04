b43ae4794a536149dcf3ca22242911f72cfefd96
dev1 <dev1@email.com>
2024-01-04 11:41:50 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -3,6 +3,25 @@ variable "ami-id" {
   default =  "ami-03265a0778a880afb"
 }
 
+# data "aws_ami" "centos8" {
+#   owners = ["973714476881"]
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["Centos-8-DevOps-Practice"]
+#   }
+#   filter {
+#     name   = "sroot-device-type"
+#     values = ["EBS"]
+#   }
+#   filter {
+#     name   = "Virtualization type"
+#     values = ["hvm"]
+#   }
+
+# }
+
 variable "instance_name" {
   type    =  list
   default =  ["mongodb","mysql","shipping","user","cart","reddis","rabbitmq","catalogue","payment","web","dispatch"]
